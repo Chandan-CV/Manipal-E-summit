cae66d3ebcc44638c975c5737d2a287daf0c74f4
Neat11 <hello.astitva@gmail.com>
2023-03-08 01:16:58 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Amita': ['Amita', 'cursive'],
+        'Monoton': ['Monoton','cursive'],
+        'Playfair': ['Playfair Display', 'serif']
+      }
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.scrollbar-hide': {
+          /* IE and Edge */
+          '-ms-overflow-style': 'none',
+
+          /* Firefox */
+          'scrollbar-width': 'none',
+
+          /* Safari and Chrome */
+          '&::-webkit-scrollbar': {
+            display: 'none'
+          }
+        }
+      }
+      )
+    })
+  ],
+}
